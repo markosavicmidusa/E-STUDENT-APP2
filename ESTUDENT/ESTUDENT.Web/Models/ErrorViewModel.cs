14cc8cee3a892eddee44dd7fcfd714cfce997ee4
Marko Savic <marko@minorityprogrammers.org>
2022-03-27 18:18:56 +0200

    making a project

---
+++ ESTUDENT/ESTUDENT.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESTUDENT.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
